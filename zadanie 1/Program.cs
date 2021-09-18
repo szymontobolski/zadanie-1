50bf541019311929c26f64556171420907886e72
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-18 09:02:50 +0200

    Add project files.

---
+++ zadanie 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jan";
+            Console.WriteLine("imię:" + name);
+            string surname = "Nowak";
+            Console.WriteLine("nazwisko:" + surname);
+            int age = 49;
+            Console.WriteLine("wiek:" + age);
+            string sex = "mężczyzna";
+            Console.WriteLine("płeć:" + sex);
+            long pesel = 14397503421;
+            Console.WriteLine("pesel:" + pesel);
+            long id = 2509324095;
+            Console.WriteLine("numer pracownika:" + id);
+
+
+            
+        }
+    }
+}
